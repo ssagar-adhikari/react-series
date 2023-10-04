fba2ea09f57cdcf609a3c4892820422119529b46
Sagar Adhikari <ssagar1916@gmail.com>
2023-10-04 07:49:22 +0545

    react project with cra and vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World | Sagar Adhikari</h1>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  
+
+  return (
+    <h1>React With Vite</h1>
+  )
+}
+
+export default App
